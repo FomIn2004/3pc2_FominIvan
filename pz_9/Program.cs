8e4027e01eae62d7f9ca9e8e6c2a99ece31ef4f6
FomIn2004 <91515536+FomIn2004@users.noreply.github.com>
2023-04-25 13:42:09 +0500

    Add files via upload

---
+++ pz_9/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace pz_009
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            #region microwave
+
+            //Microwave toshiba = new();
+            //toshiba.Time = 1.0f;
+            //toshiba.Mode = 2;
+            //toshiba.Start(new Food() { Name = "egg" });
+
+            #endregion
+
+            Client a = new();
+            a.Show('o');
+            a.Show(9);
+            a.Show(2.0f);
+        }
+    }
+}
